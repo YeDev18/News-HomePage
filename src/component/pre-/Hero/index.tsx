cb6319baa020dcffa477ebae7893c4f1cdbe7ec9
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-03-25 18:37:00 +0000

    Edit Hero

--- src/component/pre-/Hero/index.tsx
+++ src/component/pre-/Hero/index.tsx
@@ -2,16 +2,16 @@ import Img1 from '../../../assets/images/image-web-3-desktop.jpg';
 import { newItems } from '../../../data';
 const Hero = () => {
   return (
-    <div className="container max-w-auto px-4 flex flex-col gap-8 lg:flex-row lg:px-8">
-      <div className=" flex flex-col lg:w-10/12">
+    <div className="container max-w-auto px-4 flex flex-col gap-x-4 gap-y-8 lg:flex-row lg:px-8">
+      <div className=" flex flex-col gap-y-1 lg:w-[65%]">
         <div>
           <img src={Img1} alt="" />
         </div>
-        <div className="flex flex-col justify-between  py-8 lg:flex-row lg:gap-8">
-          <h1 className=" text-[#00001a] text-[3.5rem] leading-[3.7rem] font-bold tracking-[0.01em]">
+        <div className="flex flex-col justify-between py-4 lg:flex-row lg:gap-8">
+          <h1 className=" text-[#00001a] text-[2.5rem] leading-[3.7rem] font-bold tracking-[0.01em] lg:text-[3.5rem]">
             The Bright Future of Web 3.0 ?
           </h1>
-          <div className=" flex flex-col gap-8  w-[45rem]">
+          <div className=" flex flex-col gap-8  lg:w-[45rem]">
             <p className="text-[#c5c6ce] text-sm leading-6 tracking-[0.04em] font-normal">
               We dive into the next evolution of the web that claims to put the
               power of the platforms back into the hands ot the people. But is
@@ -24,14 +24,17 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col bg-[#00001a] p-8 ">
+      <div className="flex flex-col gap-8 bg-[#00001a] p-8 h-fit ">
         <h1 className="text-[#e9ab53] text-4xl font-bold tracking-[0.01em] ">
           New
         </h1>
-        <div>
+        <div className="flex flex-col gap-8 lg:gap-4 ">
           {newItems.map((newItem: any, index: number) => {
             return (
-              <div key={index}>
+              <div
+                key={index}
+                className="py-8 flex flex-col gap-2 leading-[1.5rem] tracking-[0.01em] even:border-y-[0.1px] even:border-[#fffdfa67]"
+              >
                 <h1 className="text-[#fffdfa] text-xl font-bold">
                   {newItem.h1}
                 </h1>
